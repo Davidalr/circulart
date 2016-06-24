2398ba459b02da899d77238a49f4fc2b52233cb3
Juan2Ramos <juan2ramos>
2016-06-23 22:53:45 -0500

    pdf

--- wp-content/themes/Circulart/page-versiones.php
+++ wp-content/themes/Circulart/page-versiones.php
@@ -14,12 +14,12 @@
         <h2>VERSIONES ANTERIORES</h2>
         <hr>
         <ul class="row center">
-            <li class="col-2"><a href="" target="_blank">2010</a></li>
-            <li class="col-2"><a href="" target="_blank">2011</a></li>
-            <li class="col-2"><a href="" target="_blank">2012</a></li>
-            <li class="col-2"><a href="" target="_blank">2013</a></li>
-            <li class="col-2"><a href="" target="_blank">2014</a></li>
-            <li class="col-2"><a href="" target="_blank">2015</a></li>
+            <li class="col-2"><a href="http://circulart.org/2016/wp-content/uploads/2016/06/Catalogo_MCM_2010.pdf" target="_blank">2010</a></li>
+            <li class="col-2"><a href="http://circulart.org/2016/wp-content/uploads/2016/06/Catalogo_MCM_2011_flipping.pdf" target="_blank">2011</a></li>
+            <li class="col-2"><a href="#" target="_blank">2012</a></li>
+            <li class="col-2"><a href="http://circulart.org/2016/wp-content/uploads/2016/06/Catalogo_2013_31_Oct.pdf" target="_blank">2013</a></li>
+            <li class="col-2"><a href="http://circulart.org/2016/wp-content/uploads/2016/06/CATALOGO_2014_alta.pdf" target="_blank">2014</a></li>
+            <li class="col-2"><a href="http://circulart.org/2016/wp-content/uploads/2016/06/CATALOGO_CIRCULART_2015.pdf" target="_blank">2015</a></li>
         </ul>
     </section>
 
