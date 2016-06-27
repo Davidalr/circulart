e37e78421526bd718f1a4aa3cbb4be25831e1d12
Juan2Ramos <juan2ramos>
2016-06-27 11:31:52 -0500

    mas austes

--- wp-content/themes/Circulart/index.php
+++ wp-content/themes/Circulart/index.php
@@ -21,28 +21,20 @@
 <section><iframe width="100%" height="315" src="https://www.youtube.com/embed/pPvtio-7fSE" frameborder="0" allowfullscreen></iframe></section>
 <section class="row Gallery center">
     <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/1img.png" alt="">
+        <a href="https://www.flickr.com/photos/134400696@N07/"><img src="<?php bloginfo('template_url') ?>/assets/images/1img.png" alt=""></a>
     </figure>
     <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/2img.png" alt="">
+          <a href="https://www.flickr.com/photos/134400696@N07/"><img src="<?php bloginfo('template_url') ?>/assets/images/2img.png" alt=""></a>
     </figure>
     <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/3img.png" alt="">
+          <a href="https://www.flickr.com/photos/134400696@N07/"><img src="<?php bloginfo('template_url') ?>/assets/images/3img.png" alt=""></a>
     </figure>
     <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/4img.png" alt="">
+          <a href="https://www.flickr.com/photos/134400696@N07/"><img src="<?php bloginfo('template_url') ?>/assets/images/4img.png" alt=""></a>
     </figure>
     <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/5img.png" alt="">
+          <a href="https://www.flickr.com/photos/134400696@N07/"><img src="<?php bloginfo('template_url') ?>/assets/images/5img.png" alt=""></a>
     </figure>
-    <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/6img.png" alt="">
-    </figure>
-    <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/7img.png" alt="">
-    </figure>
-    <figure>
-        <img src="<?php bloginfo('template_url') ?>/assets/images/1img.png" alt="">
-    </figure>
+
 </section>
 <?php get_footer(); ?>
