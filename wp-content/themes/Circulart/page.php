47ba37c9fe3167399113f55cfef24d0a94467854
Juan2Ramos <juan2ramos>
2016-06-27 09:53:16 -0500

    aliados

--- wp-content/themes/Circulart/page.php
+++ wp-content/themes/Circulart/page.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 
-
+<?php if( is_page( array( 'aliados' )) ):?>
+    <section class="row inside">
+        <hr>
+        <article class="col-9">
+            <h2>ORGANIZA</h2>
+            <img src="<?php bloginfo('template_url') ?>/assets/images/LogosAliados.png" alt="">
+        </article>
+    </section>
+<?php endif ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <section class="row inside">
         <hr>
