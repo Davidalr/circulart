0c1c97d6f0857ce6fc84fb0e1b2288218058f0a7
Juan2Ramos <juan2ramos>
2016-07-11 09:18:54 -0500

    Inscripción Profesionales

--- wp-content/themes/Circulart/header.php
+++ wp-content/themes/Circulart/header.php
@@ -117,7 +117,7 @@
                 <!--<li class="txt greenBack"><a href="<?php /*bloginfo('url') */?>/versiones" >Circulart</a></li>-->
                 <li class="txt"><a href="<?php bloginfo('url') ?>/calendario">Calendario</a></li>
                 <li class="txt"><a href="http://circulart.org/2016/mercado/">Convocatoria</a></li>
-                <li class="txt"><a href="http://circulart.org/2016/profesionales/">Inscripción <br> Profesionales</a></li>
+                <li style="text-align: right" class="txt"><a href="http://circulart.org/2016/profesionales/">Inscripción<br>Profesionales</a></li>
 
             </ul>
         </div>
