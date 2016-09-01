140c64b875fb2a1ff34ed5009a272147d17523d8
Juan2Ramos <juan2ramos>
2016-09-01 00:08:00 -0500

    responsive

--- wp-content/themes/Circulart/assets/js/main.js
+++ wp-content/themes/Circulart/assets/js/main.js
@@ -5,3 +5,8 @@ $('#contact').on('click', function () {
     $( ".PopUpBlock" ).show( "slow" );
     return false;
 });
+$('.Menu-mobile').on('click', function () {
+    $( ".Nav-mobile" ).toggleClass( "show" );
+    $( this ).toggleClass( "show" );
+    return false;
+});
--- wp-content/themes/Circulart/header.php
+++ wp-content/themes/Circulart/header.php
@@ -32,6 +32,68 @@
         <li><a id="contact" href="#">Contacto</a></li>
     </ul>
 </div>
+<div class="Menu-mobile">
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+<div class="Nav-mobile row center middle">
+    <ul class="center">
+        <li><a href="<?php bloginfo('url') ?>">Inicio</a></li>
+        <li><a href="<?php bloginfo('url') ?>/noticias">Noticias</a></li>
+        <li><a href="<?php bloginfo('url') ?>/medellin">Medellín</a></li>
+        <li><a href="<?php bloginfo('url') ?>/aliados">Aliados</a></li>
+        <li><a href="<?php bloginfo('url') ?>/versiones" >CIRCULART</a></li>
+        <li><a href="<?php bloginfo('url') ?>/festival-showcase">FESTIVAL <b>SHOWCASE</b></a></li>
+        <li><a href="<?php bloginfo('url') ?>/zona-de-negocios">ZONA DE <b>NEGOCIOS</b></a></li>
+        <li><a href="<?php bloginfo('url') ?>/zona-academica">ZONA <b>ACADÉMICA</b></a></li>
+        <li class="txt"><a href="<?php bloginfo('url') ?>/calendario">Calendario</a></li>
+        <li class="txt"><a href="http://circulart.org/2016/mercado/">Convocatoria</a></li>
+        <li ><a href="http://circulart.org/2016/profesionales/">Inscripción Profesionales</a></li>
+        <li class="row around Nav-mobileSocial">
+            <a href="https://www.facebook.com/Circulart" target="_blank">
+
+                <svg width="15px" height="31px" viewBox="0 -1 15 31" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <!-- Generator: Sketch 39.1 (31720) - http://www.bohemiancoding.com/sketch -->
+                    <desc>Created with Sketch.</desc>
+                    <defs></defs>
+                    <path d="M9.6197,29.1155 L3.6537,29.1155 L3.6537,14.6945 L0.6717,14.6945 L0.6717,9.7235 L3.6537,9.7235 L3.6537,6.7425 C3.6537,2.6875 5.3367,0.2755 10.1207,0.2755 L14.0967,0.2755 L14.0967,5.2465 L11.6107,5.2465 C9.7487,5.2465 9.6267,5.9425 9.6267,7.2375 L9.6197,9.7235 L14.1267,9.7235 L13.5997,14.6945 L9.6197,14.6945 L9.6197,29.1155 L9.6197,29.1155 L9.6197,29.1155 Z" id="Stroke-3" stroke="#FFFFFF" stroke-width="0.8" fill="none"></path>
+                </svg>
+            </a>
+            <a href="https://twitter.com/Circulart_" target="_blank">
+
+                <svg width="33px" height="28px" viewBox="-1 -1 33 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <!-- Generator: Sketch 39.1 (31720) - http://www.bohemiancoding.com/sketch -->
+                    <desc>Created with Sketch.</desc>
+                    <defs></defs>
+                    <path d="M2.3297,2.1107 C2.3297,2.1107 6.7497,8.2977 14.8917,8.4217 C14.8917,8.4217 14.1997,3.0557 18.8737,1.4177 C18.8737,1.4177 22.8507,-0.2253 25.4997,2.8667 C25.4997,2.8667 28.5977,2.3637 29.2927,1.4177 C29.2927,1.4177 29.1007,3.3167 26.9537,4.7017 C26.9537,4.7017 28.9117,4.6977 30.3007,3.9417 C30.3007,3.9417 28.3477,6.3837 27.5017,6.7657 C27.4887,6.7717 27.4737,6.7787 27.4607,6.7827 C26.6397,7.1027 28.3477,12.5947 23.2287,19.0947 C23.2287,19.0947 19.3547,24.3357 12.1147,25.3487 C9.1737,25.7587 4.8547,26.1057 0.4987,22.8227 C0.4987,22.8227 4.9137,23.3897 9.3367,20.1067 C9.3367,20.1067 4.7907,19.7277 3.7207,15.9377 C3.7207,15.9377 5.8007,15.9407 6.2427,15.8767 C6.2427,15.8767 1.3827,14.2327 1.4457,9.5927 C1.4457,9.5927 3.0857,10.3177 4.0037,10.2227 C4.0037,10.2227 -0.1043,7.1587 2.3297,2.1107 L2.3297,2.1107 L2.3297,2.1107 Z" id="Stroke-1" stroke="#FFFFFF" stroke-width="0.8" fill="none"></path>
+                </svg>
+            </a>
+            <a href="https://www.instagram.com/circulartmc/" target="_blank">
+
+                <svg width="32px" height="32px" viewBox="-1 -1 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <!-- Generator: Sketch 39.1 (31720) - http://www.bohemiancoding.com/sketch -->
+                    <desc>Created with Sketch.</desc>
+                    <defs></defs>
+                    <g id="Group" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <path d="M27.2281,20.2845 C27.2281,24.0745 24.1551,27.1495 20.3611,27.1495 L9.8731,27.1495 C6.0821,27.1495 3.0071,24.0745 3.0071,20.2845 L3.0071,9.7955 C3.0071,6.0035 6.0821,2.9295 9.8731,2.9295 L20.3611,2.9295 C24.1551,2.9295 27.2281,6.0035 27.2281,9.7955 L27.2281,20.2845 L27.2281,20.2845 L27.2281,20.2845 Z M21.4831,0.3395 L8.7511,0.3395 C4.1491,0.3395 0.4171,4.0725 0.4171,8.6745 L0.4171,21.4065 C0.4171,26.0075 4.1491,29.7405 8.7511,29.7405 L21.4831,29.7405 C26.0861,29.7405 29.8181,26.0075 29.8181,21.4065 L29.8181,8.6745 C29.8181,4.0725 26.0861,0.3395 21.4831,0.3395 L21.4831,0.3395 L21.4831,0.3395 Z" id="Stroke-7" stroke="#FFFFFF" stroke-width="0.8"></path>
+                        <path d="M15.1168,20.348 C12.1848,20.348 9.8078,17.972 9.8078,15.04 C9.8078,12.108 12.1848,9.732 15.1168,9.732 C18.0498,9.732 20.4258,12.108 20.4258,15.04 C20.4258,17.972 18.0498,20.348 15.1168,20.348 L15.1168,20.348 L15.1168,20.348 Z M15.1168,7.874 C11.1588,7.874 7.9508,11.082 7.9508,15.04 C7.9508,18.998 11.1588,22.206 15.1168,22.206 C19.0748,22.206 22.2838,18.998 22.2838,15.04 C22.2838,11.082 19.0748,7.874 15.1168,7.874 L15.1168,7.874 L15.1168,7.874 Z" id="Stroke-9" stroke="#FFFFFF" stroke-width="0.8"></path>
+                        <path d="M23.2746,5.9811 C24.0046,5.9811 24.5946,6.5711 24.5946,7.2991 C24.5946,8.0271 24.0046,8.6171 23.2746,8.6171 C22.5476,8.6171 21.9566,8.0271 21.9566,7.2991 C21.9566,6.5711 22.5476,5.9811 23.2746,5.9811 L23.2746,5.9811 Z" id="Stroke-11" stroke="#FFFFFF" stroke-width="0.8"></path>
+                    </g>
+                </svg>
+            </a>
+            <a href="https://www.youtube.com/channel/UCp1LtBBVxj68-drbIhJg71Q" target="_blank">
+
+                <svg width="40px" height="28px" viewBox="-1 0 40 28" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <!-- Generator: Sketch 39.1 (31720) - http://www.bohemiancoding.com/sketch -->
+                    <desc>Created with Sketch.</desc>
+                    <defs></defs>
+                    <path d="M14.4947,19.9612 L14.4947,7.3942 C18.5217,9.4972 22.5237,11.5832 26.5757,13.6982 C22.5347,15.7932 18.5337,17.8662 14.4947,19.9612 L14.4947,19.9612 L14.4947,19.9612 Z M37.7407,5.0822 C37.2757,3.0592 35.6247,1.5692 33.6327,1.3472 C28.9237,0.8202 24.1537,0.8182 19.4047,0.8202 C14.6577,0.8182 9.8867,0.8202 5.1757,1.3472 C3.1847,1.5692 1.5347,3.0592 1.0687,5.0822 C0.4077,7.9582 0.3997,11.1022 0.3997,14.0642 C0.3997,17.0242 0.3997,20.1672 1.0617,23.0442 C1.5267,25.0672 3.1787,26.5572 5.1687,26.7792 C9.8827,27.3062 14.6507,27.3082 19.3967,27.3062 C24.1467,27.3082 28.9177,27.3062 33.6257,26.7792 C35.6147,26.5572 37.2697,25.0672 37.7337,23.0442 C38.3967,20.1672 38.3997,17.0242 38.3997,14.0642 C38.3997,11.1022 38.4037,7.9582 37.7407,5.0822 L37.7407,5.0822 L37.7407,5.0822 Z" id="Stroke-5" stroke="#FFFFFF" stroke-width="0.8" fill="none"></path>
+                </svg>
+            </a>
+        </li>
+    </ul>
+</div>
 <main>
 
     <header class="Header"
